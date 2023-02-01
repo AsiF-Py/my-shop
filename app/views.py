a454a96f53c611d27f1b38f6325c55aa4815be64
AsiF-Py <asif.mannanuddin@gmail.com>
2023-02-02 00:31:15 +0600

    Hello

--- app/views.py
+++ app/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render,HttpResponse
+from .models import Product
+# Create your views here
+def product_list(request,category_slug=None):
+    # product_list = Product.objects.filter(available=True)
+    return render(request,'base.html')
+    # return HttpResponse("Hello")
